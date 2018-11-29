bd9315e585a5c608c288fae0ff7f28cf727e9b1f
Carlos Moreno <carlooos_28@hotmail.com>
2018-11-29 11:24:35 -0500

    Create layout.blade.php

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,16 @@
+# resources/views/products/layout.blade.php
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 5.7 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
